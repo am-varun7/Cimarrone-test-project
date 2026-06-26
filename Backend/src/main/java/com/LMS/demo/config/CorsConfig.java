8d5fc1f56558d0fad7ad6edd61a97c9fce1a3f0a
am-varun7 <maitreyavarun5@gmail.com>
2026-06-26 11:56:01 +0530

    deployment cors test-1

--- Backend/src/main/java/com/LMS/demo/config/CorsConfig.java
+++ Backend/src/main/java/com/LMS/demo/config/CorsConfig.java
@@ -1,15 +1,19 @@
 package com.LMS.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import java.util.List; 
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -56,3 +60,4 @@ public class CorsConfig {
         return source;
     }
 }
+
